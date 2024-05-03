4953daf65b7655e8ab9471dc1ec93ac3f56a6821
Ilanna Freire <ilannafreiret@hotmail.com>
2024-05-03 02:51:52 -0300

    feat: add aula variaveis

---
+++ fundamentosjava/AulaVariaveis.java
@@ -0,0 +1,25 @@
+package fundamentosjava;
+
+public class AulaVariaveis {
+    public static void main(String[] args) {
+        int minhaIdade = 40;
+        int suaIdade = 22;
+
+        int totalIdades = minhaIdade + suaIdade;
+
+        System.out.println(totalIdades);
+
+    }
+}
+
+/*
+int minhaIdade = 22; outra possibilidade
+use lowerCamelCase
++ é usado para somar ou concatenar info para imprimir na tela.
+é recomendado, no Code Convencion da Oracle, declarar uma variável em cada linha
+ */
+
+
+
+
+
