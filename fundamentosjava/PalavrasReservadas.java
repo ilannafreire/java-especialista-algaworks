8b17d3b5ba11887cdef018694af67a9b527eec13
Ilanna Freire <ilannafreiret@hotmail.com>
2024-05-03 02:52:31 -0300

    feat: add palavras reservadas

---
+++ fundamentosjava/PalavrasReservadas.java
@@ -0,0 +1,21 @@
+package fundamentosjava;
+
+public class PalavrasReservadas {
+}
+
+// Palavras reservadas no java que não podem ser atribuidas
+// dentro do código.
+
+
+/*
+* Abstract , assert, boolean, break, byte,
+* case, catch, char , class, const, continue
+* default, do, double, else, extends
+* final, finally , float, for, goto
+* if, implements , import , instanceof, int
+* interface, long, native, new , package
+* private, protect, public, return, short
+* static, strictfp, super , switch, synchronized,
+* this, throw, throws, transient , try
+* void, volatile, while, enum.
+ */
