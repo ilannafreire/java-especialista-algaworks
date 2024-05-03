92101f16074a6f81a85ee06bd64b9dc092f2e21a
Ilanna Freire <ilannafreiret@hotmail.com>
2024-05-03 02:52:16 -0300

    feat: add praticas de codigo

---
+++ fundamentosjava/ConvencoesDeCodigo.java
@@ -0,0 +1,13 @@
+package fundamentosjava;
+
+public class ConvencoesDeCodigo {
+}
+
+
+
+/* Atente-se às convenções de código.
+Duas mais conhecidas: Google Java Style Guide e
+Code Convention for Java programming Language, da Oracle
+* https://google.github.io/styleguide/javaguide.html
+* https://www.oracle.com/java/technologies/javase/codeconventions-contents.html
+ */
