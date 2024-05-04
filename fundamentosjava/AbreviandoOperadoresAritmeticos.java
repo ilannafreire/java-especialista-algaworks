2ac9cd642623eabc03f05351239cb0c00720d544
Ilanna Freire <ilannafreiret@hotmail.com>
2024-05-04 07:10:24 -0300

    feat: atualizando abreviação de operadores

--- fundamentosjava/AbreviandoOperadoresAritmeticos.java
+++ fundamentosjava/AbreviandoOperadoresAritmeticos.java
@@ -4,12 +4,24 @@ public class AbreviandoOperadoresAritmeticos {
     public static void main(String[] args) {
         int quantidadeDeProdutos = 10;
 
+        quantidadeDeProdutos = quantidadeDeProdutos + 3;
+
+        //quantidadeDeProdutos += 3;
+
+        //quantidadeDeProdutos -= 3;
+
+        //quantidadeDeProdutos *= 2;
+
+        //quantidadeDeProdutos /= 4;
+
+
 
         System.out.println(quantidadeDeProdutos);
     }
 }
 
 
-/* 
-*
+/* A linha 7 é o mesma coisa da linha 9.
+* l9- usamos um operador de atribuição que faz a soma da propria
+* variável que ta recebendo essa atribuição.
  */
